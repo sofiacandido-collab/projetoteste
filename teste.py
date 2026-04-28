9707ac3fe653d1057620ba07143fd843c9a07bd6
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-04-28 15:03:31 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title('Teste ECMI 2')
 st.write("Esse é o meu texto")
+
+st.image("https://www.petz.com.br/blog/wp-content/uploads/2017/07/filhote-gato-01.webp", caption="Imagem da Internet")
