15413500eeea5ba11570ed6581849e67a69be9da
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-04-28 15:11:22 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,4 +5,4 @@ st.write("Esse é o meu texto")
 
 st.image("https://www.petz.com.br/blog/wp-content/uploads/2017/07/filhote-gato-01.webp", caption="Imagem da Internet")
 
-st.video("https://youtu.be/1QS9jbvlEU8?si=BwqiKjLc9OVh2vCu", caption="Vídeo do youtube")
+st.video("https://youtu.be/1QS9jbvlEU8?si=BwqiKjLc9OVh2vCu")
