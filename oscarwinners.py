7651e940da54c5755307ab6c2be08d25721932f0
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-04-28 15:47:02 -0300

    Update oscarwinners.py

--- oscarwinners.py
+++ oscarwinners.py
@@ -8,7 +8,8 @@ st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxZxR57Uvr_NcwcJ
 st.write("-----------------------------------------")
 st.markdown('Melhor Filme')
 st.markdown("""Indicados: 
-- O Agente Secreto Uma Batalha Após a Outra 
+- O Agente Secreto 
+- Uma Batalha Após a Outra 
 - Bugonia
 - F1: O Filme
 - Frankenstein
@@ -18,3 +19,16 @@ st.markdown("""Indicados:
 - Valor Sentimental
 - Sonhos de Trem 
 """)
+
+st.write("Vencedor: Uma Batalha Aós a Outra")
+st.markdown("""Sobre esse filme: 
+- Título original: One Battle After Another
+- Diretor: Paul Thomas Anderson
+- Roteiro: Paul Thomas Anderson
+- Elenco principal: Leonardo DiCaprio, Sean Penn, Regina Hall
+- Data de lançamento: Previsto para 2025
+- Gênero: Drama / Crime
+- Inspiração: Baseado no romance Vineland, de Thomas Pynchon
+- Ambientação: Estados Unidos, com contexto político e social
+- Produção: Warner Bros. Pictures
+- Tema central: Conflitos pessoais e políticos, com foco em personagens lidando com sistemas de poder e resistência
