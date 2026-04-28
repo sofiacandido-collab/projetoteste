f4861771f745d9e4d5c9e7d195324456cc4cc21d
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-04-28 15:09:59 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,3 +4,6 @@ st.title('Teste ECMI 2')
 st.write("Esse é o meu texto")
 
 st.image("https://www.petz.com.br/blog/wp-content/uploads/2017/07/filhote-gato-01.webp", caption="Imagem da Internet")
+
+https://youtu.be/1QS9jbvlEU8?si=BwqiKjLc9OVh2vCu
+st.video("https://youtu.be/1QS9jbvlEU8?si=BwqiKjLc9OVh2vCu", caption="Vídeo do youtube")
