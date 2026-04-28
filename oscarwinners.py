c9dff0d4683d3d2472ecad8447bea332928f1480
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-04-28 15:35:33 -0300

    Update oscarwinners.py

--- oscarwinners.py
+++ oscarwinners.py
@@ -6,5 +6,5 @@ st.write("Essa página reúne todos os vencedores dos Oscars 2026")
 st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxZxR57Uvr_NcwcJQFpG7jNeWGav36sy6w-g&s")
 
 st.write("-----------------------------------------")
+st.markdown('Melhor Filme')
 
-st.markdown('<small>Melhor Filme</small>', unsafe_allow_html=True)
