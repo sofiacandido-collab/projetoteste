5f399a841ca3acf40bd37dfa7b2f0721ad348791
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-04-29 21:14:47 -0300

    Update teatromusical.py

--- teatromusical.py
+++ teatromusical.py
@@ -7,7 +7,7 @@ st.image("https://i.pinimg.com/1200x/86/c3/33/86c333dde2d4a0d270c319f36e8e073d.j
 
 st.write("-----------------------------------------")
 st.markdown('Afinal, o que é teatro musical?')
-st.markdown(""O teatro musical combina música, dança e representação em uma única performance, contando histórias por meio desses elementos."")
+st.markdown("O teatro musical combina música, dança e representação em uma única performance, contando histórias por meio desses elementos.")
 st.markdown(""A primeira obra que se aproxima do conceito do teatro musical moderno é datada de 
 1728 e se chama A Ópera do Mendigo, composta por John Gay. Entretanto, podemos dizer que o marco inicial do teatro musical foi *Show Boat*, em 1927, música de Jerome David Kern e libreto de Oscar Hammerstein, o primeiro grande espetáculo americano nesse gênero. "") 
 
