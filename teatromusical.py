c9ba1a4a38f57523445884e19502f505c0e2c099
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-04-29 21:06:51 -0300

    Update and rename oscarwinners.py to teatromusical.py

---
+++ teatromusical.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title('Teatro Musical 101 - Guia para iniciantes!')
+st.write("Tudo que você precisa saber para explorar esse universo!")
+
+st.image("https://i.pinimg.com/1200x/86/c3/33/86c333dde2d4a0d270c319f36e8e073d.jpg", caption="Legenda da imagem", width=300)
+
+st.write("-----------------------------------------")
+st.markdown('Afinal, o que é teatro musical?')
+st.markdown(""O teatro musical combina música, dança e representação em uma única performance, contando histórias por 
+meio desses elementos.
+A primeira obra que se aproxima do conceito do teatro musical moderno é datada de 
+1728 e se chama A Ópera do Mendigo, composta por John Gay. Entretanto, podemos dizer que o marco inicial do 
+teatro musical foi “Show Boat”, em 1927, música de Jerome David Kern e libreto de Oscar Hammerstein, 
+o primeiro grande espetáculo americano nesse gênero. "") 
+
+st.markdown('E no Brasil:')
+st.write(""O teatro musical chegou ao Brasil no final do século XIX, inicialmente com o “Teatro de Revista”, 
+contribuindo para criar uma identidade nacional nos palcos. A primeira adaptação de uma peça da Broadway 
+foi “Minha Querida Lady” (My Fair Lady), estrelada por Bibi Ferreira e Paulo Autran."")
+st.markdown"")
