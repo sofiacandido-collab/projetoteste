f5f7cc736fa55ddd060fc97c2c6c408cfc7bd674
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-04-29 21:24:15 -0300

    Update teatromusical.py

--- teatromusical.py
+++ teatromusical.py
@@ -3,19 +3,19 @@ import streamlit as st
 st.title('Teatro Musical 101 - Guia para iniciantes!')
 st.write("Tudo que você precisa saber para explorar esse universo!")
 
-st.image("https://i.pinimg.com/1200x/86/c3/33/86c333dde2d4a0d270c319f36e8e073d.jpg", caption="Legenda da imagem", width=300)
+st.image("https://i.pinimg.com/1200x/86/c3/33/86c333dde2d4a0d270c319f36e8e073d.jpg", caption="Elenco original da produção de Hamilton da Broadway", width=300)
 
 st.write("-----------------------------------------")
-st.markdown('Afinal, o que é teatro musical?')
-st.write("O teatro musical combina música, dança e representação em uma única performance, contando histórias por meio desses elementos.")
+st.markdown('**Afinal, o que é teatro musical?**')
 st.markdown("""
 O teatro musical combina música, dança e representação em uma única performance,
 contando histórias por meio desses elementos.
 """)
 
-st.markdown('E no Brasil:')
+
+st.markdown('**E no Brasil:**')
 st.write("""
-O teatro musical chegou ao Brasil no final do século XIX, inicialmente com o *Teatro de Revista*, 
+O teatro musical chegou ao Brasil no final do século XIX, inicialmente com o *T*eatro** **de** **Revista**, 
 um tipo de espetáculo leve e satírico que misturava música, dança e esquetes para comentar 
 acontecimentos e costumes da época.
 """)
