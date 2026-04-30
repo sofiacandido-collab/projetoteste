d8bacb8d2c2ac4e8a722be629f72940ec175ae8b
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-04-29 22:12:10 -0300

    Update teatromusical.py

--- teatromusical.py
+++ teatromusical.py
@@ -148,3 +148,13 @@ df = pd.DataFrame(dados)
 df.set_index("Aspecto", inplace=True)
 st.dataframe(df, use_container_width=True)
 st.info("A Broadway e West End são os dois maiores centros de teatro musical do mundo.")
+
+st.markdown('**Premiações importantes:**')
+st.write("""
+🏆 Tony Awards
+principal prêmio da Broadway
+🏆 Olivier Awards
+principal do West End
+""")
+
+st.info("EGOT: Alguém que é vencedor de um Emmy, Grammy, Oscar e um TONY")
