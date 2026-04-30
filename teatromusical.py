9a6d7fdf4ff9d2cdf2589d5c23c16ef5841c867d
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-04-29 21:15:15 -0300

    Update teatromusical.py

--- teatromusical.py
+++ teatromusical.py
@@ -8,11 +8,11 @@ st.image("https://i.pinimg.com/1200x/86/c3/33/86c333dde2d4a0d270c319f36e8e073d.j
 st.write("-----------------------------------------")
 st.markdown('Afinal, o que é teatro musical?')
 st.markdown("O teatro musical combina música, dança e representação em uma única performance, contando histórias por meio desses elementos.")
-st.markdown(""A primeira obra que se aproxima do conceito do teatro musical moderno é datada de 
-1728 e se chama A Ópera do Mendigo, composta por John Gay. Entretanto, podemos dizer que o marco inicial do teatro musical foi *Show Boat*, em 1927, música de Jerome David Kern e libreto de Oscar Hammerstein, o primeiro grande espetáculo americano nesse gênero. "") 
+st.markdown("A primeira obra que se aproxima do conceito do teatro musical moderno é datada de 
+1728 e se chama A Ópera do Mendigo, composta por John Gay. Entretanto, podemos dizer que o marco inicial do teatro musical foi *Show Boat*, em 1927, música de Jerome David Kern e libreto de Oscar Hammerstein, o primeiro grande espetáculo americano nesse gênero.") 
 
 st.markdown('E no Brasil:')
-st.write(""O teatro musical chegou ao Brasil no final do século XIX, inicialmente com o *Teatro de Revista*, um tipo de espetáculo leve e satírico que misturava música, dança e esquetes para comentar acontecimentos e costumes da época. 
+st.write("O teatro musical chegou ao Brasil no final do século XIX, inicialmente com o *Teatro de Revista*, um tipo de espetáculo leve e satírico que misturava música, dança e esquetes para comentar acontecimentos e costumes da época. 
 contribuindo para criar uma identidade nacional nos palcos. A primeira adaptação de uma peça da Broadway 
-foi *Minha Querida Lady* (My Fair Lady), estrelada por Bibi Ferreira e Paulo Autran."")
+foi *Minha Querida Lady* (My Fair Lady), estrelada por Bibi Ferreira e Paulo Autran.")
 
