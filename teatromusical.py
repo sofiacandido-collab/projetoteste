43b7553d5ea9f0abf9893b96eb0378f6ca2ff48d
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-04-29 21:35:21 -0300

    Update teatromusical.py

--- teatromusical.py
+++ teatromusical.py
@@ -17,5 +17,37 @@ st.markdown('**E no Brasil:**')
 st.write("""
 O teatro musical chegou ao Brasil no final do século XIX, inicialmente com o *T*eatro** **de** **Revista**, 
 um tipo de espetáculo leve e satírico que misturava música, dança e esquetes para comentar 
-acontecimentos e costumes da época.
+acontecimentos e costumes da época, A primeira adaptação de uma peça da Broadway foi 
+Minha Querida Lady (My Fair Lady), com Bibi Ferreira e Paulo Autran e, no fim dos anos 60, o teatro nacional 
+era divido entre adaptações de grandes espetáculos como Jesus Cristo Superstar e Rocky Horror Show, mas também contou 
+com produções nacionais, como Roda Viva e Ópera do Malandro.
+""")
+
+
+st.markdown('**Como funciona uma peça de teatro?**)
+st.write("Quando chega o horário de começar a peça são tocados três sinais, em que o último representa o início da peça. 
+Elas são divididas em três partes, o primeiro ato, intervalo e o segundo ato.
+
+with st.expander("O que acontece no Ato 1?"):
+    st.write("""
+O Ato 1 é responsável por apresentar a história.
+
+Aqui você vai ver:
+- Introdução dos personagens principais  
+- Explicação do contexto da história  
+- Surgimento do conflito principal  
+
+Geralmente termina com um momento marcante que deixa o público curioso para o que vem depois.
+""")
+with st.expander("O que você encontra no Ato 2?"):
+    st.write("""
+O Ato 2 desenvolve e resolve a história.
+
+Aqui você vai ver:
+- Evolução do conflito  
+- Momentos emocionais mais intensos  
+- Clímax (ponto mais importante da história)  
+- Desfecho final  
+
+É onde tudo se resolve.
 """)
