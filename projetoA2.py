1b56f81fb6e6dbf40a5d4f21343b2723e89f3656
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-05-19 15:28:51 -0300

    Update projetoA2.py

--- projetoA2.py
+++ projetoA2.py
@@ -1,1 +1,15 @@
-
+framelens/
+│
+├── app.py
+├── requirements.txt
+├── utils/
+│   ├── scraper.py
+│   ├── sentiment.py
+│   ├── ai_analysis.py
+│
+├── database/
+│   └── supabase.py
+│
+├── assets/
+│
+└── README.md
